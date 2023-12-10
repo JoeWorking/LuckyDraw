ea1703253c3903cad57b2b8d1d9c773a44fe593d
Kwan Kit Chung <kwankit0629@gmail.com>
2023-12-11 01:14:31 +0800

    Add UsersModel and Controller

---
+++ Controller/UsersController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import fetch from "node-fetch";
+import { usersModel } from "../Model/UsersModel";
+
+export class UsersController {
+  constructor(private usersModel: usersModel) {}
+
+  getAllUsers = async (req: Request, res: Response) => {
+    try {
+      const allUsers = await this.usersModel.getAllUsers();
+      res.json(allUsers);
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ error: "An error occurred while fetching users." });
+    }
+  };
+}
---
+++ Model/usersModel.ts
@@ -0,0 +1,48 @@
+import { Knex } from "knex";
+
+interface User {
+  UserId: string;
+  UserName: string;
+  Password: string;
+  DepartmentCode: string;
+  Chance: number;
+  IsAdmin: boolean;
+  created_by: string;
+  mod_by: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export class usersModel {
+  constructor(private knex: Knex) {}
+
+  async getAllUsers(): Promise<User[]> {
+    try {
+      const users = await this.knex.select("*").from("Users");
+      return users;
+    } catch (error) {
+      // Handle the error appropriately
+      console.error(error);
+      throw error;
+    } finally {
+      // Don't forget to destroy the Knex connection
+      this.knex.destroy();
+    }
+  }
+  async loginChecking(userId: string): Promise<User[]> {
+    try {
+      const users = await this.knex("Users")
+        .select("*")
+        .where("userId", userId);
+
+      return users;
+    } catch (error) {
+      // Handle the error appropriately
+      console.error(error);
+      throw error;
+    } finally {
+      // Don't forget to destroy the Knex connection
+      this.knex.destroy();
+    }
+  }
+}
